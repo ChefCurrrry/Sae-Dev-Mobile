0d17d9a701d359ac0445596a740318eb130fa3de
Kiran <k.kichenassamy@yahoo.com>
2025-03-31 12:37:28 +0200

    Taff

--- app/(tabs)/statistiques.tsx
+++ app/(tabs)/statistiques.tsx
@@ -66,7 +66,7 @@ const styles = StyleSheet.create({
     container: {
         paddingHorizontal: 20,
         paddingTop: 30,
-        marginRight: 20,
+        marginRight: 40,
     },
     title: {
         fontSize: 18,
